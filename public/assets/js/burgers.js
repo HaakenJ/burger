149864673c5baa021ca3783bd633b12ef1d9853a
Haakasaurus <kramerhjohnson@gmail.com>
2019-12-02 12:55:47 -0800

    Converted to using sass file.  Changed page styling.

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -25,21 +25,23 @@ $(function () {
         // Make sure to preventDefault on a submit event.
         event.preventDefault();
 
-        var newBurger = {
-            name: $("#ca").val().trim(),
-            devoured: $("[name=devoured]:checked").val().trim()
-        };
+        if ($("#ca").val()) {
+            var newBurger = {
+                name: $("#ca").val().trim(),
+                devoured: $("[name=devoured]:checked").val().trim()
+            };
 
-        // Send the POST request.
-        $.ajax("/api/burgers", {
-            type: "POST",
-            data: newBurger
-        }).then(
-            function () {
-                console.log("created new burger");
-                // Reload the page to get the updated list
-                location.reload();
-            }
-        )
+            // Send the POST request.
+            $.ajax("/api/burgers", {
+                type: "POST",
+                data: newBurger
+            }).then(
+                function () {
+                    console.log("created new burger");
+                    // Reload the page to get the updated list
+                    location.reload();
+                }
+            )
+        }
     })
 })
