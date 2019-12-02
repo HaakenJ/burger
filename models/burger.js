f138c5545c01b627d46f8b44c4cea0f85f51f7dd
Haakasaurus <kramerhjohnson@gmail.com>
2019-12-02 10:22:17 -0800

    Created burger.js methods

--- models/burger.js
+++ models/burger.js
@@ -1,2 +1,21 @@
 const orm = require("../config/orm");
 
+const burger = {
+    all: function(cb) {
+        orm.selectAll("burgers", res => {
+            cb(res);
+        })
+    },
+    create: function(cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, res => {
+            cb(res);
+        })
+    },
+    update: function(objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, res => {
+            cb(res);
+        })
+    }
+};
+
+module.exports = burger;
