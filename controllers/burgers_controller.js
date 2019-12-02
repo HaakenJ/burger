10337de78dc699530db5ae54aee7a0518d1fcae5
Haakasaurus <kramerhjohnson@gmail.com>
2019-12-02 10:49:27 -0800

    Completed put route and export.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -22,3 +22,21 @@ router.post("/api/burgers", (req, res) => {
         res.json({ id: result.insertId });
     })
 })
+
+router.put("/api/burgers/:id", (req, res) => {
+    const objColVals = {
+        devoured: req.body.devoured
+    };
+    const condition = `id=${req.params.id}`;
+    console.log(`Condition: ${condition}`);
+
+    burger.update(objColVals, condition, result => {
+        if (result.changedRows === 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    })
+})
+
+module.exports = router;
