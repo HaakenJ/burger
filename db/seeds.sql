a19a46eb84b02a81ff2e41735b430d21e9924e86
Haakasaurus <kramerhjohnson@gmail.com>
2019-12-02 11:26:41 -0800

    Completed front end js and added seeds.

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured) VALUES
+("Chicken Bacon", false),
+("Impossible", true),
+("Mushroom Swiss", false),
+("Southwest Onion", false),
+("Super Bacon", true),
+("Veggie", true),
+("Black Bean", false);
