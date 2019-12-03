dc03d255e806a7ab9b8589532f3d98ffd29a32f7
Haakasaurus <kramerhjohnson@gmail.com>
2019-12-03 13:42:37 -0800

    Added JAWSDB connection.  Moved MySQL password to .env

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
+require("dotenv").config();
 const mysql = require("mysql");
+let connection;
 
-const connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection ({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "kramer",
+    password: process.env.MYSQL_PASS,
     database: "burgers_db"
-});
+    });
+}
 
 connection.connect(err => {
     if (err) {
